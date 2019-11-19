2de5f119841335b691f09096c7313fdabd5d46b1
tryToCode <dong.yang@campus.tu-berlin.de>
2019-11-19 12:35:20 +0100

    small layout changes

--- pages/about.vue
+++ pages/about.vue
@@ -1,23 +1,30 @@
 <template>
-  <div class="max-w-5xl flex flex-col mx-auto justify-center px-4 py-2">
+  <div class="max-w-3xl flex flex-col mx-auto justify-center px-4 py-2">
       <div class="my-2">
           <h1 class="text-2xl">About Podcasts Zone</h1>
-          <p>
-              Podcasts Zone is constantly updated podcast aggregator inspired from Alltop.com <br>
-                We focus on the topics IT, Entrepreneurship and Finance.
-                All data come from the Internet, if you see any violiations of your rights in any form,
-                please contact us using the contact form.
+          <p class="pt-2">
+            Podcasts Zone is constantly updated podcast aggregator inspired from 
+            <a class="border-b-2" href="https://alltop.com">Alltop.com</a>  
+            <br>
+            We focus on the topics IT, Entrepreneurship and Finance.
+            <br>
+            All data come from the Internet, if you see any violiations of your rights in any form,
+            please <nuxt-link to="/contact" class="border-b-2">Contact</nuxt-link> us.
           </p>
       </div>
       <div class="items-center">
-          <h1 class="text-2xl">Podcast we currently covered: </h1>
+          <h1 class="text-2xl pb-2">Podcasts we currently cover: </h1>
           <div v-for="podcast in podcasts"
-            :key="podcast.id">
-            <a :href="podcast.base_url">{{podcast.name}}</a>
+            :key="podcast.id"
+            class="pb-1">
+            <a :href="podcast.base_url"
+            class="border-b-2">
+                {{podcast.name}}
+            </a>
           </div>
       </div>
       <div class="my-2">
-          <p class="font-semibold">If you have any thoughs, feel free to let us know. </p>
+          <p class="font-semibold">You host a podcast and want to be on the list, let us know. </p>
       </div>
   </div>
 </template>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="max-w-3xl flex flex-col mx-auto justify-center px-4 py-2">
         <div class="flex items-center justify-between my-2">
-            <h1 class="text-3xl font-semibold">Contact Us</h1>
+            <h1 class="text-3xl font-semibold">Get in Touch</h1>
         </div>
         <form class="w-full">
             <div class="flex flex-col mb-2">
