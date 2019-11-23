38e89e4975acdaa35a7895550c9fe3adfdddcf60
tryToCode <dong.yang@campus.tu-berlin.de>
2019-11-23 23:29:59 +0100

    add postcss

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,6 +66,24 @@ export default {
     ** You can extend webpack config here
     */
     extend (config, ctx) {
+    },
+
+    postcss: {
+      // Add plugin names as key and arguments as value
+      // Install them before as dependencies with npm or yarn
+      plugins: {
+        // Disable a plugin by passing false as value
+        'postcss-url': false,
+        'postcss-nested': {},
+        'postcss-responsive-type': {},
+        'postcss-hexrgba': {}
+      },
+      preset: {
+        // Change the postcss-preset-env settings
+        autoprefixer: {
+          grid: true
+        }
+      }
     }
   },
 }
