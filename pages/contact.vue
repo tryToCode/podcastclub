a30856a71333ae63dae2666a60c90009e95fbe6d
tryToCode <dong.yang@campus.tu-berlin.de>
2019-11-26 16:01:21 +0100

    add info in contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="max-w-3xl flex flex-col mx-auto justify-center px-4 py-2">
+        <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+            <div class="flex">
+                <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+                <div>
+                <p class="font-bold">This page is still under development and may not working correctly</p>
+                </div>
+            </div>
+        </div>
         <div class="flex flex-col items-center justify-between my-2">
             <h1 class="text-3xl font-semibold my-1">Contact Page</h1>
             <p>
