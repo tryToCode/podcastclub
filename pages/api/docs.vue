e35c2cd1fc87126dc9103c2e5d9baca92c39a533
tryToCode <dong.yang@campus.tu-berlin.de>
2019-11-26 20:56:41 +0100

    update docs page

--- pages/api/docs.vue
+++ pages/api/docs.vue
@@ -1,12 +1,53 @@
 <template>
-    <div class="max-w-5xl bg-gray-100 flex flex-col mx-auto p-2 justify-center items-center overflow-hidden">
+    <div class="max-w-3xl flex flex-col mx-auto p-2 justify-center overflow-hidden">
         <div class="m-2">
-            <h1 class="text-2xl">Podcast Club consumes data from the podcast backend Api </h1>
+            <h1 class="text-2xl font-medium m-2">Intro</h1>
+            <p class="text-base m-2">
+                Podcast Club consumes data from the 
+                <a class="border-b-2" href="https://github.com/tryToCode/podcast-api">
+                podcast backend Api</a> which is build with the
+                <a class="border-b-2" href="https://www.django-rest-framework.org/">Django Rest API</a>.
+                The backend Api contains mainly 2 Resources as described below and currently only support
+                HTTP GET method. 
+            </p>
+            <p class="m-2">
+                <strong>Note</strong>: The base Url to the backend Api: http://fathomless-beyond-28426.herokuapp.com
+            </p>
         </div>
-        <div class="flex flex-col border-2 p-4 m-2">
-            <h1 class="text-2xl font-extrabold m-2">Items</h1>
+        <div class="flex flex-col m-2">
+            <h1 class="text-2xl font-medium m-2">Resources</h1>
+            <p>
+                <span class="text-green-400 m-2">api/rssItems</span>
+                <span class="">3412 items</span>
+            </p>
+            <p>
+                <span class="text-green-400 m-2">api/podcasts</span>
+                <span class="">16 podcasts</span>
+            </p>
+        </div>
+        <div class="flex flex-col m-2">
+            <h1 class="text-2xl font-medium m-2">Routes</h1>
+            <p>
+                <span class="py-1 m-2 uppercase">get</span>
+                <span class="text-green-400">api/rssItems</span>
+            </p>
+            <p>
+                <span class="py-1 m-2 uppercase">get</span>
+                <span class="text-green-400">api/rssItems/1</span>
+            </p>
+            <p>
+                <span class="py-1 m-2 uppercase">get</span>
+                <span class="text-green-400">api/podcasts</span>
+            </p>
+            <p>
+                <span class="py-1 m-2 uppercase">get</span>
+                <span class="text-green-400">api/podcasts/1</span>
+            </p>
+        </div>
+        <div class="flex flex-col border-2 p-4 m-4">
+            <h1 class="text-2xl font-bold m-2">Items</h1>
             <div>
-                <span class="inline border-2 bg-green-300 px-2 py-1 rounded uppercase m-2">Get</span>
+                <span class="font-bold border-2 text-green-400 px-2 py-1 rounded uppercase m-2">Get</span>
                 <span class="bg-green-100">http://fathomless-beyond-28426.herokuapp.com/api/rssItems</span>
             </div>
             <div class="m-2">
@@ -15,10 +56,10 @@
                 </vue-json-pretty>                
             </div>
         </div>
-        <div class="flex flex-col border-2 p-4 m-2">
-            <h1 class="text-2xl font-extrabold m-2">Single Item</h1>
+        <div class="flex flex-col border-2 p-4 m-4">
+            <h1 class="text-2xl font-bold m-2">Single Item</h1>
             <div>
-                <span class="inline border-2 bg-green-300 px-2 py-1 rounded uppercase m-2">Get</span>
+                <span class="font-bold border-2 text-green-400 px-2 py-1 rounded uppercase m-2">Get</span>
                 <span class="bg-green-100">http://fathomless-beyond-28426.herokuapp.com/api/rssItems/:id</span>
             </div>
             <div class="m-2">
@@ -27,10 +68,10 @@
                 </vue-json-pretty>                
             </div>
         </div>
-        <div class="flex flex-col border-2 p-4 m-2">
-            <h1 class="text-2xl font-extrabold m-2">Podcasts</h1>
+        <div class="flex flex-col border-2 p-4 m-4">
+            <h1 class="text-2xl font-bold m-2">Podcasts</h1>
             <div>
-                <span class="inline border-2 bg-green-300 px-2 py-1 rounded uppercase m-2">Get</span>
+                <span class="font-bold border-2 text-green-400 px-2 py-1 rounded uppercase m-2">Get</span>
                 <span class="bg-green-100">http://fathomless-beyond-28426.herokuapp.com/api/podcasts</span>
             </div>
             <div class="m-2">
@@ -39,10 +80,10 @@
                 </vue-json-pretty>                
             </div>
         </div>
-        <div class="flex flex-col border-2 p-4 m-2">
-            <h1 class="text-2xl font-extrabold m-2">Single Podcast</h1>
+        <div class="flex flex-col border-2 p-4 m-4">
+            <h1 class="text-2xl font-bold m-2">Single Podcast</h1>
             <div>
-                <span class="inline border-2 bg-green-300 px-2 py-1 rounded uppercase m-2">Get</span>
+                <span class="font-bold border-2 text-green-400 px-2 py-1 rounded uppercase m-2">Get</span>
                 <span class="bg-green-100">http://fathomless-beyond-28426.herokuapp.com/api/podcasts/:id</span>
             </div>
             <div class="m-2">
@@ -58,6 +99,16 @@
 import { mapState } from 'vuex'
 
 export default {
+    head () {
+        return {
+            title: "Api Docs",
+            meta: [
+                { name: 'description', 
+                  content: 'Podcast club Podcast Club backend Api documetation' }
+            ]
+        }
+    },
+
     created() {
         this.$store.dispatch('loadItems', 'http://fathomless-beyond-28426.herokuapp.com/api/rssItems')
         this.$store.dispatch('loadPodcasts')
