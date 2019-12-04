88739e0a1c4489385e7c4d65de630ed866ff3840
tryToCode <dong.yang@campus.tu-berlin.de>
2019-12-04 18:31:46 +0100

    change help text

--- pages/help.vue
+++ pages/help.vue
@@ -12,26 +12,27 @@
             <li>Podcast original URL</li>
             </ul>
           <p class="py-1">
-              Check out <nuxt-link class="border-b-2" to="/about">here</nuxt-link> to see availiable Podcasts.
+              Check out <nuxt-link class="border-b-2" to="/about">here</nuxt-link> to see available Podcasts.
           </p>
       </div>
       <div class="my-2">
           <h1 class="text-2xl">Filter</h1>
           <p class="py-1 font-bold">
-              Known Issue: No Items Found
+              Possible Filter Result: No Items Found
           </p>
           <p>
             Items are filtered by the given filter input which may include 
-            <ul class="list-disc list-inside">
-                <li>Category of item</li>
-                <li>Published date of item</li>
-                <li>The result page number</li>
+            <ul class="list-disc list-inside py-1">
+                <li>Category of Item</li>
+                <li>Published date of Item</li>
+                <li>The page number, the <span class="font-bold">default page number</span> is 1</li>
             </ul>
-            The result page number could cause the search result "No Items Found".
+            The page number (Pagination number at the bottom) could cause the result "No Items Found".
           </p>
           <p class="py-1 font-bold">Solution</p>
           <p>
-            Make sure the filter "Category" or "Date" is excuted without querying the page number.
+            Make sure the filter "Category" or "Date" is executed by querying with 
+            the default page number <span class="font-bold">1</span>.
           </p>
       </div>
   </div>
