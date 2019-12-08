d33034ec708566d99ab5c06f13c16df3b41e0f76
tryToCode <dong.yang@campus.tu-berlin.de>
2019-12-08 15:04:01 +0100

    add dummy voting

--- components/BaseItemRow.vue
+++ components/BaseItemRow.vue
@@ -1,53 +1,65 @@
 <template>
-    <div class="overflow-hidden py-1">
-        <div class="px-6">
-            <a v-if="item.description.length !== 0"
-                class="font-medium text-base"
-                :href="item.item_url"
-                :content="item.description"
-                v-tippy="{ placement : 'top', arrow: true, size: 'large' }">
-            {{item.title}}
-            </a>
-            <a v-else
-                class="font-medium text-base"
-                :href="item.item_url">
-            {{item.title}}
-            </a>
-            <span class="hidden md:inline text-gray-500 text-sm">
-                ({{item.item_url}})
-            </span>
+    <div class="flex overflow-hidden py-1">
+        <div class="flex flex-col justify-center pl-2 pr-1 text-center hover:text-gray-500 cursor-pointer">
+            <BaseUpArrow />
+            <span class="text-gray-500 text-sm">0</span>
         </div>
-        <div class="px-6 text-sm font-semibold text-gray-500">
-            <a class="pr-1"
-                :href="'https://' + item.creator.base_url">
-            {{item.creator.name}}
-            </a>
-            <span class="text-gray-500">|</span>
-            <span class="px-1">
-            {{item.creator.category.split('.')[1]}}
-            </span>
-            <span class="text-gray-500">|</span>
-            <span class="px-1">
-            {{$moment(item.pub_date).fromNow()}}
-            </span>
-            <div v-if="item.episode_number !== null"
-                class="hidden md:inline">
+        <div>
+            <div class="pb-1">
+                <a v-if="item.description.length !== 0"
+                    class="font-medium text-base"
+                    :href="item.item_url"
+                    :content="item.description"
+                    v-tippy="{ placement : 'top', arrow: true, size: 'large' }">
+                {{item.title}}
+                </a>
+                <a v-else
+                    class="font-medium text-base"
+                    :href="item.item_url">
+                {{item.title}}
+                </a>
+                <span class="hidden md:inline text-gray-500 text-sm">
+                    ({{item.item_url}})
+                </span>
+            </div>
+            <div class="text-sm font-semibold text-gray-500">
+                <a class="pr-1"
+                    :href="'https://' + item.creator.base_url">
+                {{item.creator.name}}
+                </a>
                 <span class="text-gray-500">|</span>
                 <span class="px-1">
-                    #{{item.episode_number}}
+                {{item.creator.category.split('.')[1]}}
                 </span>
+                <span class="text-gray-500">|</span>
+                <span class="px-1">
+                {{$moment(item.pub_date).fromNow()}}
+                </span>
+                <div v-if="item.episode_number !== null"
+                    class="hidden md:inline">
+                    <span class="text-gray-500">|</span>
+                    <span class="px-1">
+                        #{{item.episode_number}}
+                    </span>
+                </div>
             </div>
         </div>
     </div>
 </template>
     
 <script>
+import BaseUpArrow from './BaseUpArrow.vue'
+
 export default {
     props: {
         item: {
             type: Object,
             required: true
         }
+    },
+
+    components: {
+        BaseUpArrow
     }
 }
 </script>
---
+++ components/BaseUpArrow.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="pointer-events-none flex items-center justify-center px-2 text-gray-400">
+        <svg class="fill-current h-4 w-4" 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 24 24">
+            <path d="M0 15.878 l12-11.878 12 11.878-4 4.122-8-8-8 8-4-4.122z"></path>
+        </svg>
+    </div>
+</template>
