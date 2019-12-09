a1a4cdea3a8616cc0686136091c7abf957f9ab66
tryToCode <dong.yang@campus.tu-berlin.de>
2019-12-09 17:23:43 +0100

    change padding for setting select option

--- pages/settings.vue
+++ pages/settings.vue
@@ -5,14 +5,14 @@
           <h1 class="text-xl border-b-2">Display Options</h1>
           <div class="flex flex-col flex-wrap text-sm">
             <div class="flex flex-wrap justify-center items-center m-4">
-                <label class="hidden md:block md:px-2 tracking-wide text-gray-700" 
+                <label class="px-2 tracking-wide text-gray-700" 
                     for="grid-state">
                     Episodes Per Page
                 </label>
                 <div class="relative">
                     <select class="block appearance-none bg-gray-200 
                             border border-gray-200 text-gray-700 
-                            px-1 py-1 md:px-2 md:pr-8 rounded leading-tight focus:outline-none 
+                            px-1 py-1 pr-8 md:px-2 md:pr-8 rounded leading-tight focus:outline-none 
                             focus:bg-white focus:border-gray-500" 
                         name="selectOptions"
                         @change="onFilterChange($event)"
